719757c3e8cec00d9f47f49a00264d898697b21f
omkar <omkar.c.344@gmail.com>
2022-06-15 18:19:30 +0530

    added hardhat config file

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+      },
+      {
+        version: "0.8.4",
+      },
+      {
+        version: "0.8.1",
+      },
+    ],
+  },
+};
