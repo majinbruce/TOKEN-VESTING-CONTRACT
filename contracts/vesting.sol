7a7c20211ea09374f348b6f87bff9a659cd561be
omkar <omkar.c.344@gmail.com>
2022-06-23 13:00:41 +0530

    updated project structure

--- contracts/vesting.sol
+++ contracts/vesting.sol
@@ -4,7 +4,6 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract vesting is Ownable, ReentrancyGuard {
@@ -13,33 +12,9 @@ contract vesting is Ownable, ReentrancyGuard {
     bytes32 public constant MENTOR = keccak256("Mentor");
 
     using SafeERC20 for IERC20;
-
     IERC20 private immutable token;
     uint256 private immutable oneMonthInSeconds = 2629743;
     uint256 private immutable oneDayInSeconds = 86400;
-
-    // role => address of role owner => total available tokens
-    mapping(bytes32 => mapping(address => uint256))
-        public availableTokensPerRoleAndAddress;
-
-    // benificiary => vesting schedule of the address
-    mapping(bytes32 => mapping(address => VestingSchedule))
-        public VestingSchedulePerRoleAndAddress;
-    // tracks available token percentage per role
-    mapping(bytes32 => uint256) public availableTokenPercentagePerRole;
-
-    struct VestingSchedule {
-        address beneficiary;
-        bytes32 role;
-        bool isVested;
-        uint256 createVestingScheduleTime;
-        uint256 start;
-        uint256 cliff;
-        uint256 duration;
-        uint256 percentageOfTotalSupply;
-        uint256 totalTokensReleased;
-        uint256 lastClaim;
-    }
 
     event vestingScheduleCreated(
         address beneficiary,
@@ -56,20 +31,34 @@ contract vesting is Ownable, ReentrancyGuard {
         uint256 amountOfTokensClaimed
     );
 
+    struct VestingSchedule {
+        address beneficiary;
+        bytes32 role;
+        bool isVested;
+        uint256 createVestingScheduleTime;
+        uint256 start;
+        uint256 cliff;
+        uint256 duration;
+        uint256 percentageOfTotalSupply;
+        uint256 totalTokensReleased;
+        uint256 lastClaim;
+    }
+
+    // role => address of role owner => total available tokens
+    mapping(bytes32 => mapping(address => uint256))
+        public availableTokensPerRoleAndAddress;
+
+    // benificiary => vesting schedule of the address
+    mapping(bytes32 => mapping(address => VestingSchedule))
+        public VestingSchedulePerRoleAndAddress;
+    // tracks available token percentage per role
+    mapping(bytes32 => uint256) public availableTokenPercentagePerRole;
+
     constructor(IERC20 _token) {
         token = _token;
         availableTokenPercentagePerRole[ADVISOR] = 3;
         availableTokenPercentagePerRole[PARTNER] = 5;
         availableTokenPercentagePerRole[MENTOR] = 4;
-    }
-
-    function getCreatedSchedule(string memory _role, address beneficiary)
-       external
-        view
-        returns (VestingSchedule memory)
-    {
-        bytes32 roleinString = getRoleInBytesFromString(_role);
-        return VestingSchedulePerRoleAndAddress[roleinString][beneficiary];
     }
 
     function calculateTotalTokensReleased(uint256 _percentageOfTotalSupply)
@@ -81,52 +70,20 @@ contract vesting is Ownable, ReentrancyGuard {
     }
 
     function getRoleInBytesFromString(string memory _role)
-       public
+        public
         pure
         returns (bytes32)
     {
         return keccak256(abi.encodePacked(_role));
     }
 
-    function preVestingScheduleValidation(
-        address _beneficiary,
-        bytes32 _roleInBytes,
-        uint256 _percentageOfTotalSupply
-    ) internal view {
-        VestingSchedule
-            storage vestingSchedule = VestingSchedulePerRoleAndAddress[
-                _roleInBytes
-            ][_beneficiary];
-
-        require(
-            !vestingSchedule.isVested,
-            "VESTING : tokens for this address and role already vested"
-        );
-        require(
-            _beneficiary != address(0x00),
-            "VESTING: benificiary cannot be 0 address"
-        );
-
-        require(
-            _roleInBytes == ADVISOR ||
-                _roleInBytes == PARTNER ||
-                _roleInBytes == MENTOR,
-            "VESTING: enter correct role"
-        );
-
-        require(
-            _percentageOfTotalSupply <=
-                availableTokenPercentagePerRole[_roleInBytes],
-            "VESTING: cannot allocate entered percentage of tokens for this role"
-        );
-    }
-
-    function convertFromMonthToSeconds(uint256 _time)
-        internal 
-        pure
-        returns (uint256)
+    function getCreatedSchedule(string memory _role, address beneficiary)
+        external
+        view
+        returns (VestingSchedule memory)
     {
-        return _time * (oneMonthInSeconds);
+        bytes32 roleinString = getRoleInBytesFromString(_role);
+        return VestingSchedulePerRoleAndAddress[roleinString][beneficiary];
     }
 
     function createVestingSchedule(
@@ -194,65 +151,6 @@ contract vesting is Ownable, ReentrancyGuard {
         );
     }
 
-    function calculateTimeElapsed(bytes32 _roleInBytes, address _beneficiary)
-       internal
-        view
-        returns (uint256)
-    {
-        VestingSchedule
-            storage vestingSchedule = VestingSchedulePerRoleAndAddress[
-                _roleInBytes
-            ][_beneficiary];
-
-        uint256 timeElapsed;
-        uint256 currentTime = block.timestamp;
-
-        if (vestingSchedule.lastClaim == 0) {
-            timeElapsed = currentTime - vestingSchedule.cliff;
-        } else {
-            timeElapsed = currentTime - vestingSchedule.lastClaim;
-        }
-
-        return (timeElapsed);
-    }
-
-    function prereleaseVestedTokensValidation(
-        address _beneficiary,
-        bytes32 _roleInBytes
-    ) internal  view {
-        VestingSchedule
-            storage vestingSchedule = VestingSchedulePerRoleAndAddress[
-                _roleInBytes
-            ][_beneficiary];
-
-        require(
-            vestingSchedule.lastClaim == 0 ||
-                vestingSchedule.lastClaim <=
-                block.timestamp - vestingSchedule.lastClaim,
-            "VESTING : you can only claim tokens every 24hours"
-        );
-        require(
-            vestingSchedule.isVested,
-            "VESTING : tokens for this address and role are not vested"
-        );
-
-        bool isBeneficiary = msg.sender == vestingSchedule.beneficiary;
-        bool isOwner = msg.sender == owner();
-        require(
-            isBeneficiary || isOwner,
-            "VESTING: only beneficiary and owner can claim vested tokens"
-        );
-
-        require(
-            _roleInBytes == ADVISOR ||
-                _roleInBytes == PARTNER ||
-                _roleInBytes == MENTOR,
-            "VESTING: enter correct role"
-        );
-
-        require(block.timestamp >= vestingSchedule.cliff);
-    }
-
     function releaseVestedTokens(
         address _beneficiary,
         string memory _roleInString
@@ -296,5 +194,105 @@ contract vesting is Ownable, ReentrancyGuard {
 
         token.safeTransfer(_beneficiary, tokensToRelease);
         emit tokensReleased(_beneficiary, _roleInBytes, tokensToRelease);
+    }
+
+    function preVestingScheduleValidation(
+        address _beneficiary,
+        bytes32 _roleInBytes,
+        uint256 _percentageOfTotalSupply
+    ) internal view {
+        VestingSchedule
+            storage vestingSchedule = VestingSchedulePerRoleAndAddress[
+                _roleInBytes
+            ][_beneficiary];
+
+        require(
+            !vestingSchedule.isVested,
+            "VESTING : tokens for this address and role already vested"
+        );
+        require(
+            _beneficiary != address(0x00),
+            "VESTING: benificiary cannot be 0 address"
+        );
+
+        require(
+            _roleInBytes == ADVISOR ||
+                _roleInBytes == PARTNER ||
+                _roleInBytes == MENTOR,
+            "VESTING: enter correct role"
+        );
+
+        require(
+            _percentageOfTotalSupply <=
+                availableTokenPercentagePerRole[_roleInBytes],
+            "VESTING: cannot allocate entered percentage of tokens for this role"
+        );
+    }
+
+    function convertFromMonthToSeconds(uint256 _time)
+        internal
+        pure
+        returns (uint256)
+    {
+        return _time * (oneMonthInSeconds);
+    }
+
+    function calculateTimeElapsed(bytes32 _roleInBytes, address _beneficiary)
+        internal
+        view
+        returns (uint256)
+    {
+        VestingSchedule
+            storage vestingSchedule = VestingSchedulePerRoleAndAddress[
+                _roleInBytes
+            ][_beneficiary];
+
+        uint256 timeElapsed;
+        uint256 currentTime = block.timestamp;
+
+        if (vestingSchedule.lastClaim == 0) {
+            timeElapsed = currentTime - vestingSchedule.cliff;
+        } else {
+            timeElapsed = currentTime - vestingSchedule.lastClaim;
+        }
+
+        return (timeElapsed);
+    }
+
+    function prereleaseVestedTokensValidation(
+        address _beneficiary,
+        bytes32 _roleInBytes
+    ) internal view {
+        VestingSchedule
+            storage vestingSchedule = VestingSchedulePerRoleAndAddress[
+                _roleInBytes
+            ][_beneficiary];
+
+        require(
+            vestingSchedule.lastClaim == 0 ||
+                vestingSchedule.lastClaim <=
+                block.timestamp - vestingSchedule.lastClaim,
+            "VESTING : you can only claim tokens every 24hours"
+        );
+        require(
+            vestingSchedule.isVested,
+            "VESTING : tokens for this address and role are not vested"
+        );
+
+        bool isBeneficiary = msg.sender == vestingSchedule.beneficiary;
+        bool isOwner = msg.sender == owner();
+        require(
+            isBeneficiary || isOwner,
+            "VESTING: only beneficiary and owner can claim vested tokens"
+        );
+
+        require(
+            _roleInBytes == ADVISOR ||
+                _roleInBytes == PARTNER ||
+                _roleInBytes == MENTOR,
+            "VESTING: enter correct role"
+        );
+
+        require(block.timestamp >= vestingSchedule.cliff);
     }
 }
